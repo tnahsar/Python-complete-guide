5b06b6af887dfa1abdf3225089cb8f3f7355f9e2
Prashant Variyalani <prashant.variyalani@apexon.com>
2025-10-19 22:48:49 +0530

    commit

---
+++ Basic/Patterns_advanced.py
@@ -0,0 +1,44 @@
+# Problem : Butterfly Pattern (Star Version) — Most Asked in Interviews
+# *      *
+# **    **
+# ***  ***
+# ********
+# ********
+# ***  ***
+# **    **
+# *      *
+k = 6
+for i in range(1,5):
+    print("*" * i + " " * k + "*" * i)
+    k -= 2
+j = 0
+for i in range(4, 0, -1):
+    print("*" * i + " " * j + "*" * i)
+    j += 2
+
+print(" ")
+print(" ")
+
+# Problem: Hourglass Pattern (Solid and Hollow Versions)
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+a = 0
+for i in range(9, 1, -2):
+    print(" " * a + "*" * i + " " * a)
+    a += 1
+b = 4
+print(" " * b + "*")
+
+
+
+b -= 1
+for i in range(3, 10, +2):
+    print(" " * b + "*" * i)
+    b -= 1
