c4dba129d703958c41d21d4506b71f06e7384392
Prashant Variyalani <variyalaniprashant@gmail.com>
2025-10-31 16:21:41 +0530

    commit

--- Lists & Tuples/List/list_comprehensions.py
+++ Lists & Tuples/List/list_comprehensions.py
@@ -17,9 +17,23 @@ print()
 
 # Problem 3 — Create a List of Strings with Condition
 # From a list of words, create a list containing only those with length greater than 4.
+my_list = ["a", "abc", "abcd","apple", "Banana", "Papaya"]
+new_list = [s for s in my_list if len(s)> 4]
+print(new_list)
+
+print()
+print()
 
 # Problem 4 — Generate Multiplication Table (1 to 5)
 # Use nested list comprehension to generate a list of (x, y, product) tuples.
+new_list = [(n, k, n*k) for n in range(1,6) for k in range(1,11)]
+print(new_list)
+
+print()
+print()
 
 # Problem 5 — Conditional Expressions
 # Replace all odd numbers in a list with the word “Odd”, keeping even numbers unchanged.
+my_list = [1,2,3,4,5,6,7,8]
+new_list = ["Odd" if n%2 != 0 else n for n in my_list ]
+print(new_list)
---
+++ Lists & Tuples/List/list_slicing.py
@@ -0,0 +1,8 @@
+# Problem 1:
+# Demonstrate list slicing by printing sub-parts of the list (first 3 elements, last 2 elements, etc.)
+list = [1,2,4,5,6,9,2,3,5,2,8]
+print(f"{list[3:9]}")  
+
+print()
+print()
+
